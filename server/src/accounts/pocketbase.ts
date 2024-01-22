0ce561625f752d7552db7c5b002d4fa9ece0bf30
Compositr <compositr@compositr.dev>
2024-01-22 20:02:49 +0800

    feat(server): pocketbase util fns

--- server/src/accounts/pocketbase.ts
+++ server/src/accounts/pocketbase.ts
@@ -15,6 +15,10 @@ const pocketbase: Pocketbase | undefined = Config.pocketBase?.host ? new Pocketb
 
 export default {
     pocketbase,
+
+    /**
+     * Login to Pocketbase as an admin
+     */
     async login() {
         // Attempt sign in
         if (!process.env.POCKETBASE_USERNAME || !process.env.POCKETBASE_PASSWORD) throw new Error("Pocketbase username or password not specified");
@@ -23,6 +27,46 @@ export default {
         } catch (err) {
             Logger.warn("Failed to login to Pocketbase! Check host and credentials.");
             throw err;
+        }
+    },
+
+    /**
+     * Check a users credentials by attempting a login
+     *
+     * @param username Username/email to check
+     * @param password Password to check
+     *
+     * @returns User ID if valid, false if not
+     */
+    async checkCredentials(username: string, password: string) {
+        if (!pocketbase) throw new Error("Pocketbase not configured");
+        const pb = new Pocketbase(Config.pocketBase!.host);
+        try {
+            await pb.authWithPassword(username, password);
+            return pb.authStore.model.id;
+        } catch {
+            return false;
+        }
+    },
+
+    /**
+     * Check a user's auth token validity by attempting to refresh it
+     *
+     * @param token Auth token to check
+     *
+     * @returns User ID if valid, false if not
+     */
+    async checkToken(token: string) {
+        if (!pocketbase) throw new Error("Pocketbase not configured");
+        const pb = new Pocketbase(Config.pocketBase!.host);
+        pb.authStore.save(token);
+        try {
+            // Attempt to refresh token
+            // If it is not valid, throws an error
+            await pb.collection("users").authRefresh();
+            return pb.authStore.model.id;
+        } catch {
+            return false;
         }
     }
 };
